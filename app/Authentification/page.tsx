443f922e031a1b0dc02a5226d7b2f4e5f2229bda
moumen312 <abdelmoumen20007@gmail.com>
2026-04-07 16:05:03 +0100

    casued a problem

--- app/Authentification/page.tsx
+++ app/Authentification/page.tsx
@@ -1,18 +1,9 @@
 "use client";
-import { useState } from "react"
-import { supabase } from "@/app/lib/supabase"
-import { useRouter } from "next/navigation"
+
 {/* loge in page tryed to make it work does not im fix it another time  */ }
 export default function Page() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const router = useRouter()
-
-    async function handleLogin() {
-        const { error } = await supabase.auth.signInWithPassword({ email, password })
-        if (error) alert(error.message)
-        else router.push("/Product")
-    }
+   
+    
 
     return (
         <section className="min-h-screen flex items-center justify-center  ">
@@ -21,14 +12,12 @@ export default function Page() {
                 <h1 className="text-xl font-bold"> login into your acount </h1>
                 <div className="flex flex-col gap-1">
                     <label className="text-white text-sm font-medium ">Email</label>
-                    <input type="text" placeholder="Somthing@gmail.com" value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    <input type="text" placeholder="Somthing@gmail.com"
                         className="bg-[#ffff]  w-[300] h-[40] border-[#3B82F633] rounded-xl pl-4  text-black placeholder-[#94A3B8] outline-none " />
                 </div>
                 <div className="flex flex-col gap-1">
                     <label className="text-white text-sm font-medium ">Passwored</label>
-                    <input type="text" placeholder="Passowerd" value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    <input type="text" placeholder="Passowerd" 
                         className="bg-[#ffff]  w-[300] h-[40] border-[#3B82F633] rounded-xl pl-4  text-black placeholder-[#94A3B8] outline-none " />
                 </div>
                 <div className="pt-8" >
